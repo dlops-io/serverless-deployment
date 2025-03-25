cba491055ea06e1ffeaf647d03084fc46a1be387
shivasj <shivasj@gmail.com>
2025-03-25 18:15:46 -0400

    updated readme + images

--- docker-shell.sh
+++ docker-shell.sh
@@ -7,12 +7,12 @@ export IMAGE_NAME="simple-translate"
 export BASE_DIR=$(pwd)
 
 # Build the image based on the Dockerfile
-docker build -t $IMAGE_NAME -f Dockerfile .
-#docker build -t $IMAGE_NAME --platform=linux/amd64/v3 -f Dockerfile .
+#docker build -t $IMAGE_NAME -f Dockerfile .
+docker build -t $IMAGE_NAME --platform=linux/amd64/v2 -f Dockerfile .
 
 # Run Container
-docker run --rm --name $IMAGE_NAME -ti \
--v "$BASE_DIR":/app \
--p 8080:8080 \
--e DEV=1 \
-$IMAGE_NAME
+# docker run --rm --name $IMAGE_NAME -ti \
+# -v "$BASE_DIR":/app \
+# -p 8080:8080 \
+# -e DEV=1 \
+# $IMAGE_NAME
